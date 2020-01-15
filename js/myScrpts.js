59731afab2b8f0a6ecb457a98132696c1ab4b656
Baleadas123 <55458879+Baleadas123@users.noreply.github.com>
2020-01-15 13:32:34 -0500

    Update myScrpts.js

--- js/myScrpts.js
+++ js/myScrpts.js
@@ -1,1 +1,11 @@
-
+var v=2
+while(v < 8){
+  var ans= Math.random(v);
+  document.write(ans + "<br/>");
+  v++;
+}
+var k; 
+for(k=0; k<1; k++){
+  var ans= Math.sqrt(256);
+  document.write(ans + "<br/>");
+}
