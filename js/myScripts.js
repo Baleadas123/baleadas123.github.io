e57efdbe566d11eeb856bb21c7d7da0e76e441be
Baleadas123 <55458879+Baleadas123@users.noreply.github.com>
2020-01-24 13:49:22 -0500

    Update myScripts.js

--- js/myScripts.js
+++ js/myScripts.js
@@ -17,6 +17,11 @@ var end = document.getElementById("end").value;
   console.log(typeof start);
   console.log(typeof end);
   console.log(typeof jump);
+  
+  start=Number(start);
+  end=Number(end);
+  jump=Number(jump);
+  
 for(var i = start; i <= end; i+=jump)
 {document.write('<p>' + i + '</p>');
 }
